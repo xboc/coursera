58435b5807e2daada841e75bb1c4ce53e89a08e5
xboc <xboc@users.noreply.github.com>
2017-01-14 22:41:43 +0100

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,5 +1,5 @@
 (function (window) {
-        var byeSpeaker = new Object();
+        var byeSpeaker = "speaker";
         var speakWord = "Good Bye";
         byeSpeaker.speak = function (name) {
                 console.log(speakWord + " " + name);
